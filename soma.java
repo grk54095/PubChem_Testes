6ee2a0f6084063b9732caad1a01aedeb9d38bf61
grk54095 <gabrielrosakrebs@gmail.com>
2025-09-16 15:47:43 -0300

    Add files via upload

---
+++ soma.java
@@ -0,0 +1,16 @@
+public class soma {
+    public static void main(String[] args) {
+        sum(10, 5);
+    }
+
+    static void sum(int x, int y) {
+       // sum nome da função
+       //() é usado para usarmos para definir o que chegara na função
+       //{} define o corpo da função
+        System.out.println(x+y); 
+        System.out.println(x-y);
+        System.out.println(x/y);
+        System.out.println(x*y);
+
+    }
+}
