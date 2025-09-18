3c8b9db770a7a3439eccef6dc7cf8ebc2cc4e245
grk54095 <gabrielrosakrebs@gmail.com>
2025-09-18 16:05:11 -0300

    Delete oi.java
    Signed-off-by: grk54095 <gabrielrosakrebs@gmail.com>

--- oi.java
+++
@@ -1,9 +0,0 @@
-public class oi { // tem que ser o nome do arquivo
-    public static void main(String[] args) { // ponto de entrada
-        System.out.println("Hello, World!"); //as funções são chamadas com parênteses
-    }
-    void metodo() { //métodos são definidos dentro de classes
-        System.out.println("Outro método"); // tem q ter ponto e virgula    
-    }
-}
-
